aae36e385b3500933dbdb3289ce3acb96fe00ac2
Erland <erland.vo@telenet.be>
2021-05-01 23:58:23 +0200

    refactor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
+   Treehouse Techdegree:
+   FSJS Project 2 - Data Pagination and Filtering
 */
 
 
@@ -16,23 +16,39 @@ let activePage = 1;
 
 /* 
   This function adds a maximum of 'studentsPerPage' li elements with students' 
-  data taken from the 'studentData' parameter and appends it to the unordered 
+  data taken from the 'list' parameter and appends it to the unordered 
   list with the class 'student-list' in index.html.
 
   - 'studentsPerPage' is a global constant.
-  - 'studentData' is an array of objects with student data. See data.js for an 
-  example as to what 'studentData' should look like.
-  - The pages are numbered from 1. A value of 1 for the parameter 'pageNr' will 
-  display the first set of 'studentsPerPage' nr of students from the 
-  'studentData' array.
+  - 'list' is an array of objects with student data. See data.js for an 
+  example as to what 'list' should look like.
+  - The pages are numbered from 1. A value of 1 for 'page' will display 
+  the first set of 'studentsPerPage' nr of students from the 'list' array.
 */
-function showPage(studentData, pageNr) {  
+function showPage(list, page) { 
+
+   // The createLI function creates an li containing the data of the 'student'
+   // parameter
+   function createLI(student) {
+      let li = document.createElement('li');
+      li.className = 'student-item cf';
+      li.innerHTML = 
+         `<div class="student-details">` + 
+           `<img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">` +
+           `<h3>${student.name.first} ${student.name.last}</h3>` +
+           `<span class="email">${student.email}</span>` +
+         `</div>` +
+         `<div class="joined-details">` +
+            `<span class="date">Joined ${student.registered.date}</span>` +
+         `</div>`;
+      return li;
+   }
 
    // the parameter is 1 based so we subtract one to get the correct first index
-   const startIndex = (pageNr - 1) * studentsPerPage;
+   const startIndex = (page - 1) * studentsPerPage;
 
    // maked sure we are not trying to show more students that there are in the dataset
-   const endIndex = Math.min(pageNr * studentsPerPage - 1, studentData.length - 1);
+   const endIndex = Math.min(page * studentsPerPage, list.length);
 
    // select the unordered list to which we'll add the students
    const ul = document.querySelector('ul.student-list');
@@ -40,101 +56,75 @@ function showPage(studentData, pageNr) {
    // make sure we start with a empty page
    ul.innerHTML = '';
 
-   // create the li elements with the students' data
-   for(let i = startIndex; i <= endIndex; i++) {
-
-      // <li class="student-item cf">
-      let li = document.createElement('li');
-      li.className = 'student-item cf';
-
-      // <div class="student-details"></div>
-      let div = document.createElement('div');
-      div.className = 'student-details';    
-
-      // <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-      let img = document.createElement('img');
-      img.className = 'avatar';
-      img.src = studentData[i].picture.thumbnail;
-      img.alt = 'Profile Picture';
-
-      // <h3>Ethel Dean</h3>
-      let h3 = document.createElement('h3');
-      h3.innerText = `${studentData[i].name.first} ${studentData[i].name.last}`;
-
-      // <span class="email">ethel.dean@example.com</span>
-      let span = document.createElement('span');
-      span.className = 'email';
-      span.innerText = studentData[i].email;
-
-      // <div class="joined-details"></div>
-      let div2 = document.createElement('div');
-      div2.className = 'joined-details';
-
-      // <span class="date">Joined 12-15-2005</span>
-      let span2 = document.createElement('span');
-      span2.className = 'date';
-      span2.innerText = `Joined ${studentData[i].registered.date}`;
-
-      // assemble the first div
-      div.appendChild(img);
-      div.appendChild(h3);
-      div.appendChild(span);
-
-      // assemble the second div
-      div2.appendChild(span2);
-
-      // append both divs to the li
-      li.appendChild(div);
-      li.appendChild(div2);
-     
-      // append the li to the ul
+   // create the li elements with the students' data and add them to the list
+   for(let i = startIndex; i < endIndex; i++) {     
+      const li = createLI(list[i]);
       ul.appendChild(li);   
    }
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+  The addPagination function will create a list of buttons for each page (set)
+  of 'studentsPerPage' nr of students and add an event handler for these
+  buttons that call the showPage() function with the page nr the user selected.
 */
-function addPagination(studentData) {
-   const nrPages = Math.ceil(studentData.length / studentsPerPage);
+function addPagination(list) {
 
+   function createButtonLi(text, isActive) {
+      let li = document.createElement('li');
+      let className = '';
+      if(isActive) {
+         className = 'active';   
+      }
+      li.innerHTML = `<button type="button" class="${className}">${text}</button>`;
+      return li;
+   }
+
+   // The last page might not contain 9 students, hence Match.ceil()
+   const nrPages = Math.ceil(list.length / studentsPerPage);
+
+   // select the correct unordered list to which we'll add the page nr buttons
    let ul = document.querySelector('ul.link-list');
+
+   // start with 0 buttons
    ul.innerHTML = '';
 
-   for(let i = 1; i <= nrPages; i++) {
-
-      // <li>
-      let li = document.createElement('li');
-
-      // <button type="button" class="active">1</button>
-      let button = document.createElement('button');
-      button.type = 'button';
-      if(i === activePage) {
-         button.className = 'active';
-      }     
-      button.innerText = i;
-      li.appendChild(button);
+   // add the buttons for each page to the unordered list
+   for(let i = 1; i <= nrPages; i++) {      
+      const li = createButtonLi(i, i === activePage);
       ul.appendChild(li);
    } 
    
    ul.addEventListener('click', (event) => {
+
+      // only proceed if the user clicked on an actual button
+      if(event.target.tagName !== "BUTTON") {
+         return;
+      }
+
+      // extract the page/ button number from the event data
       const selectedPage = parseInt(event.target.innerText);
      
+      // reload if the user selected a page that differs from the current one
       if(selectedPage !== activePage) {
          let lis = ul.children;
+
+         // highlight the button of the currently active page
          lis[activePage - 1].querySelector('button').className = '';
          lis[selectedPage - 1].querySelector('button').className = 'active';
-         activePage = selectedPage;
-      }     
-      showPage(data, activePage);
-   });
 
+         // keep track of the currently active page
+         activePage = selectedPage;
+         
+         // and finally show the new page
+         showPage(data, activePage);
+      }        
+   });
 }
 
 // Call functions
-addPagination(data);
 showPage(data, activePage);
+addPagination(data);
 
 
 
