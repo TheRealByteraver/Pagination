a3f9ef6f18003316af01062484bccdc9295fc2e5
Erland <erland.vo@telenet.be>
2021-05-02 08:44:33 +0200

    further refactor

--- js/script.js
+++ js/script.js
@@ -11,7 +11,10 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+// How many students we want to see on a page at once:
 const studentsPerPage = 9;
+
+// We show page nr 1 by default
 let activePage = 1;
 
 /* 
@@ -28,56 +31,58 @@ let activePage = 1;
 function showPage(list, page) { 
 
    // The createLI function creates an li containing the data of the 'student'
-   // parameter
+   // parameter and returns it as a string of html code
    function createLI(student) {
-      let li = document.createElement('li');
-      li.className = 'student-item cf';
-      li.innerHTML = 
-         `<div class="student-details">` + 
-           `<img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">` +
-           `<h3>${student.name.first} ${student.name.last}</h3>` +
-           `<span class="email">${student.email}</span>` +
-         `</div>` +
-         `<div class="joined-details">` +
-            `<span class="date">Joined ${student.registered.date}</span>` +
-         `</div>`;
-      return li;
+      return (
+         `<li class="student-item cf">` +        
+            `<div class="student-details">` + 
+            `<img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">` +
+            `<h3>${student.name.first} ${student.name.last}</h3>` +
+            `<span class="email">${student.email}</span>` +
+            `</div>` +
+            `<div class="joined-details">` +
+               `<span class="date">Joined ${student.registered.date}</span>` +
+            `</div>` +
+         `</li>`
+      );
    }
 
    // the parameter is 1 based so we subtract one to get the correct first index
    const startIndex = (page - 1) * studentsPerPage;
 
-   // maked sure we are not trying to show more students that there are in the dataset
+   // maked sure we are not trying to show more students than there are in the dataset
    const endIndex = Math.min(page * studentsPerPage, list.length);
 
    // select the unordered list to which we'll add the students
    const ul = document.querySelector('ul.student-list');
 
-   // make sure we start with a empty page
+   // we start with a empty page
    ul.innerHTML = '';
 
    // create the li elements with the students' data and add them to the list
    for(let i = startIndex; i < endIndex; i++) {     
-      const li = createLI(list[i]);
-      ul.appendChild(li);   
+      const student = createLI(list[i]);
+      ul.insertAdjacentHTML('beforeend', student);
    }
 }
 
 /*
-  The addPagination function will create a list of buttons for each page (set)
-  of 'studentsPerPage' nr of students and add an event handler for these
-  buttons that call the showPage() function with the page nr the user selected.
+   The addPagination function will create a list of buttons for each page 
+   of students and add an event handler for these buttons that call the 
+   showPage() function with the page nr the user selected as parameter.
 */
 function addPagination(list) {
 
    function createButtonLi(text, isActive) {
-      let li = document.createElement('li');
       let className = '';
       if(isActive) {
          className = 'active';   
       }
-      li.innerHTML = `<button type="button" class="${className}">${text}</button>`;
-      return li;
+      return (
+         `<li>` +
+            `<button type="button" class="${className}">${text}</button>` +
+         `</li>`
+      );
    }
 
    // The last page might not contain 9 students, hence Match.ceil()
@@ -91,14 +96,16 @@ function addPagination(list) {
 
    // add the buttons for each page to the unordered list
    for(let i = 1; i <= nrPages; i++) {      
-      const li = createButtonLi(i, i === activePage);
-      ul.appendChild(li);
+      const button = createButtonLi(i, i === activePage);
+      //ul.appendChild(button);
+      ul.insertAdjacentHTML('beforeend', button);
    } 
    
+   // The below event handler listens for page-button clicks
    ul.addEventListener('click', (event) => {
 
       // only proceed if the user clicked on an actual button
-      if(event.target.tagName !== "BUTTON") {
+      if(event.target.tagName !== 'BUTTON') {
          return;
       }
 
@@ -107,6 +114,8 @@ function addPagination(list) {
      
       // reload if the user selected a page that differs from the current one
       if(selectedPage !== activePage) {
+
+         // get a list of buttons
          let lis = ul.children;
 
          // highlight the button of the currently active page
